51ada8ac9856a8cfc1d63b6417b3be0a403bc827
nelsonlachini <nelsonlachini@gmail.com>
2022-03-25 10:12:43 +0000

    auto singleload

--- Smear_radius_singleLoad.cpp
+++ Smear_radius_singleLoad.cpp
@@ -4,7 +4,7 @@ minimal compilation : clang++ ../Smear_radius_inefficient.cc -o inefficientsmear
 
 #include <Grid/Grid.h>
 #include <Hadrons/EigenPack.hpp>
-#include <Hadrons/Modules/MDistil/Distil.hpp>
+#include <Hadrons/NamedTensor.hpp>
 
 using namespace std;
 using namespace Grid;
@@ -13,6 +13,19 @@ int main (int argc, char ** argv)
 {
     // bool doMock = true;
 
+    //if (argc != 4)
+    //{
+        std::cerr << "usage: " << argv[0] << " <evec stem> <output stem> <max nvec> <config list>";
+        //std::cerr << std::endl;
+        
+        //return EXIT_FAILURE;
+    //}
+    string eigenFilestem = argv[1];
+    string outStem = argv[2];
+    int nVec_max = stoi(argv[3]);
+    vector<int> config_list = strToVec<int>(argv[4]);
+    const int step = 20;
+
     // core
     Grid_init(&argc,&argv);
     Coordinate latt_size  = GridDefaultLatt();
@@ -20,9 +33,9 @@ int main (int argc, char ** argv)
     Coordinate mpi_layout  = GridDefaultMpi();
     GridCartesian   grid(latt_size,simd_layout,mpi_layout);
     std::cout << GridLogMessage << "Lattice time extension = " << latt_size[Tdir] << std::endl;
+    std::cout << GridLogMessage << "Lattice spatial extension = " << latt_size[Xdir] << std::endl; 
     const int vol = grid.gSites();
     
-
     // temporaries
     using LapEvecs = Grid::Hadrons::EigenPack<LatticeColourVector>;
     LapEvecs eigen4d(1 , &grid);
@@ -30,37 +43,30 @@ int main (int argc, char ** argv)
     
     // file handling
     std::string eigenFilepath;
-    std::string eigenFilestem = "/tessfs1/work/dp008/dp008/dc-lach1/lapevec_new/result/eigen_rho0.200000_n12.1100.bin";
-    // std::string eigenFilestem = "/tessfs1/work/dp008/dp008/dc-lach1/lapevecs/evecs_C1_" + stout_strength;
-    // std::string eigenFilestem = "../../eigenmock";
+    //std::string eigenFilestem = "/tessfs1/work/dp008/dp008/dc-lach1/lapevec_new/result/eigen_rho0.200000_n12"; //C0
+    //std::string eigenFilestem = "t96l32_b3.80_kls0.138963_csw1.95524/evec_t96l32_b3.80_kls0.138963_csw1.95524";
     std::ofstream f;
-
+    //std::string outStem = "t96l32/";
 
     // parameters
-    std::vector<int> nvec_list = {1,4,8,12,16,24,32,48,64,88,112,128,152,200,260,310,380,450,490};
-    // std::vector<int> nvec_list = {1,2,4,6,8,10,15,20,25,30,35,40,45,50,55,60,70,80,90,100,110,120};
-    // std::vector<int> nvec_list = {1};
+    std::vector<int> nvec_list;// = {1,4,8,12,16,24,32,48,64,88,112,128,152,200,260,310,380,450,500};
+    for(int i=step;i<nVec_max;i+=step) nvec_list.push_back(i);
+    // std::vector<int> nvec_list = {1,3};
     // unsigned int TRAJECTORY_START = 1100;
     // unsigned int TRAJECTORY_END = 1481;
     // unsigned int TRAJECTORY_STEP = 20;
     // unsigned int TRAJECTORY_START = 3140;
     // unsigned int TRAJECTORY_END = 3901;
     // unsigned int TRAJECTORY_STEP = 40;
-    unsigned int TRAJECTORY_START = 1100;
-    unsigned int TRAJECTORY_END = 1460;
-    unsigned int TRAJECTORY_STEP = 40;
-    // if(doMock){
-    //     TRAJECTORY_START = 2000;
-    //     TRAJECTORY_END = 2000;
-    //     TRAJECTORY_STEP = 1;
-    //     eigenFilestem = "../../evec/eigen_test";
-    //     nvec_list = {1,5};
-    // }
+    unsigned int TRAJECTORY_START = 80;
+    unsigned int TRAJECTORY_END = 110;
+    unsigned int TRAJECTORY_STEP = 10;
     
     std::vector<std::vector<RealD>> Psi_r(latt_size[Xdir]/2 + 1 , std::vector<RealD>(nvec_list.size(), 0.0));    //final Psi
     int i_nvec;
     
-    for(unsigned int i_conf=TRAJECTORY_START ; i_conf<TRAJECTORY_END ; i_conf+=TRAJECTORY_STEP){
+    //for(unsigned int i_conf=TRAJECTORY_START ; i_conf<TRAJECTORY_END ; i_conf+=TRAJECTORY_STEP){
+    for(auto i_conf : config_list){  
         std::cout << GridLogMessage << "Starting measurement on trajectory " << i_conf << "..." << std::endl;
 
         // reseting Psi_r
@@ -83,13 +89,17 @@ int main (int argc, char ** argv)
                 outerPsi_r = Zero();
                 i_nvec = 0;
                 for(int i=0 ; i<nvec_list.back() ; i++){
-                    outerPsi_r += outerProduct(eigen4d.evec[i] , Cshift(eigen4d.evec[i] , spatial_dir , r_shift));  // v_x X v_x+r
+                    outerPsi_r += outerProduct( eigen4d.evec[i] , adj(Cshift(eigen4d.evec[i] , spatial_dir , r_shift)) ) ;  // v_x X v_x+r
+                    // cout << outerPsi_r << endl;
+                    // cin.get();
                     if(std::find(nvec_list.begin(), nvec_list.end() , i+1) != nvec_list.end()){
-                        Psi_r[r_shift][i_nvec] += toReal(TensorRemove(sum(sqrt(trace(outerPsi_r * adj(outerPsi_r))))))/(3.0*vol*(i+1)*sqrt(Nc));
-                        cout << i+1 << endl;
-                        cout << toReal(TensorRemove(sum(sqrt(trace(outerPsi_r * adj(outerPsi_r))))))/(3.0*vol*(i+1)*sqrt(Nc)) << endl;
-                        std::cout<<GridLogMessage<< "Summing out Psi (trajectory " << i_conf << ", Nvec=" << i+1 << ", r_shift=" << r_shift << ", spatial_dir=" << spatial_dir << "):" << std::endl;
+                        Psi_r[r_shift][i_nvec] += toReal(TensorRemove( sum(sqrt(trace(outerPsi_r * adj(outerPsi_r)))) ))/(3.0*vol*(i+1)*sqrt(Nc));
+                        // cout << sum(trace( outerPsi_r * adj(outerPsi_r) )) << endl;
                         // cin.get();
+                        // cout << i+1 << endl;
+                        // cout << toReal(TensorRemove(sum(sqrt(trace(outerPsi_r * adj(outerPsi_r))))))/(3.0*vol*(i+1)*sqrt(Nc)) << endl;
+                        std::cout<<GridLogMessage<< "Summing out Psi (trajectory " << i_conf << ", Nvec=" << i+1 << ", r_shift=" << r_shift << ", spatial_dir=" << spatial_dir << "):" << std::endl;
+                        // cin.get();s
                         i_nvec++;
                     }
                 }
@@ -107,12 +117,7 @@ int main (int argc, char ** argv)
 
         // writing out
         for(i_nvec=0 ; i_nvec<nvec_list.size() ; i_nvec++){
-            // output files
-            // outFilepath = "../../data/C0/" + stout_strength;
-            // string outFilepath = "../../data/C1/" + stout_strength;
-            string outFilepath = "./";
-            // if(doMock)
-            //     outFilepath = "../../data/test/";
+            string outFilepath = outStem;
             outFilepath += "/psi_nvec" + std::to_string(nvec_list[i_nvec]) + "." + std::to_string(i_conf) + ".dat";
             f.open(outFilepath);
             std::cout << GridLogMessage << "Writing to " << outFilepath << std::endl;
